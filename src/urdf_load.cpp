903d412f7476f29a19b19437ea3aae87c0a08d47
Quincy Jones <quincy@implementedrobotics.com>
2020-06-13 19:35:32 -0500

    Loading Working.

---
+++ src/urdf_load.cpp
@@ -0,0 +1,55 @@
+#include <dart/dynamics/Skeleton.hpp>
+#include <dart/dynamics/FreeJoint.hpp>
+#include <dart/dynamics/BallJoint.hpp>
+#include <dart/dynamics/BoxShape.hpp>
+#include <dart/simulation/World.hpp>
+#include <dart/utils/urdf/DartLoader.hpp>
+
+#include <dart/gui/osg/osg.hpp>
+
+
+using namespace dart::dynamics;
+using namespace dart::simulation;
+
+int main(int argc, char *argv[])
+{
+    dart::utils::DartLoader loader;
+    std::string urdf = "/home/nomad/dev/dartsim-sandbox/resource/Nomad/Nomad_leg_test.urdf";
+    SkeletonPtr leg = loader.parseSkeleton(urdf);
+    std::cout << "LOADED!" << std::endl;
+
+    auto world = World::create();
+    world->addSkeleton(leg);
+
+    ::osg::ref_ptr<dart::gui::osg::RealTimeWorldNode> node = new dart::gui::osg::RealTimeWorldNode(world);
+
+    auto viewer = dart::gui::osg::Viewer();
+    viewer.addWorldNode(node);
+    viewer.setUpViewInWindow(0,0,640,480);
+    viewer.getCameraManipulator()->setHomePosition(::osg::Vec3(2.57f, 3.14f, 1.64f),
+    ::osg::Vec3(0.0f, 0.0f, 0.0f),
+    ::osg::Vec3(-0.24f, -0.25f, 0.94f), true);
+
+    viewer.setCameraManipulator(viewer.getCameraManipulator());
+    std::cout << leg->getBodyNode("base_link")->getWorldTransform().translation();
+    std::cout << leg->getDof(0)->getName() << std::endl;
+    std::cout << leg->getDof(1)->getName() << std::endl;
+    //std::cout << leg->getDof(2)->getName() << std::endl;
+    //std::cout << leg->getDof(3)->getName() << std::endl;
+    //std::cout << leg->getDof(0)->getName() << std::endl;
+    //std::cout << leg->getDof(0)->getName() << std::endl;
+    leg->getDof(0)->setPosition(1.5);
+    leg->getDof(0)->setRestPosition(0);
+    leg->getDof(0)->setDampingCoefficient(0.2);
+    leg->getDof(1)->setDampingCoefficient(.1);//Velocity(4);
+
+    auto hip_node = leg->getBodyNode("HFE_Actuator1");
+    auto foot_node = leg->getBodyNode("Foot1");
+
+    const Eigen::MatrixXd& J = leg->getJacobian(foot_node, hip_node);
+    
+
+    std::cout << J << std::endl;
+    viewer.run();
+    return 0;
+}
